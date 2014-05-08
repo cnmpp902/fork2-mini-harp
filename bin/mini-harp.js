e4c389a32ca6988a553b99da96d2603564efd906
cnmpp902 <cnmpp902@gmail.com>
2014-05-08 13:02:41 +0800

    lesson 2

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var args = require('minimist')(process.argv.slice(2)),
     createMiniHarp = require("../"),
-    app = createMiniHarp(args._[0]);
-console.log("Starting mini-harp on http://localhost:"+args.port);
-
-app.listen(args.port);
+    port = args.port||4000,
+    app = createMiniHarp(args._[0]||"../verify/asserts");
+console.log("Starting mini-harp on http://localhost:" + port);
+app.listen(port);
--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 var connect = require('connect'),
-    serveStatic = require('serve-static');
-
-module.exports = function(path){
+    serveStatic = require('serve-static'),
+    jade = require("./lib/processor/jade"),
+    less = require("./lib/processor/less");
+module.exports = function(root){
   var app = connect();
-  if(path !== undefined){
-    app.use(serveStatic(path));
-  }
   app.use(function(req,res,next){
-    var url = req.url.split("/");
-    if(url[1] == "current-time" && url[2] === undefined){
+    //var url = req.url.split("/");
+    if(req.url == "/" || req.url == ""){
+      req.url = "/index.html";
+      next();
+    }
+    else if(req.url == "/current-time"){
       res.end((new Date()).toISOString()+"\n");
     }
     else{
       //res.end("Cannot Get "+req.url.substr(req.url.indexOf('/')));
       next();
-
       // if we not use next(), then if we write .use below, it will be useless.
       // it will stuck in else
     }
-  });
+  })
+    .use(jade(root))
+    .use(less(root))
+    .use(serveStatic(root));
   return app;
 };
