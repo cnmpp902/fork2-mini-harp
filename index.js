10b06a88bfafb9ead164e68376e5cab60e1036c8
cnmpp902 <cnmpp902@gmail.com>
2014-05-08 16:08:00 +0800

    add common.js

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 var connect = require('connect'),
+    path = require('path'),
     serveStatic = require('serve-static'),
     jade = require("./lib/processor/jade"),
     less = require("./lib/processor/less");
 module.exports = function(root){
   var app = connect();
   app.use(function(req,res,next){
-    //var url = req.url.split("/");
-    if(req.url == "/" || req.url == ""){
+    if(req.url === "/" || req.url === ""){
       req.url = "/index.html";
       next();
     }
     else if(req.url == "/current-time"){
       res.end((new Date()).toISOString()+"\n");
     }
-    else{
-      //res.end("Cannot Get "+req.url.substr(req.url.indexOf('/')));
+    else{      
       next();
       // if we not use next(), then if we write .use below, it will be useless.
       // it will stuck in else
-    }
+    }    
   })
     .use(jade(root))
     .use(less(root))
